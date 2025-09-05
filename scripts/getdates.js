f7ffd4e2959a866f79a1346c76b48456d950872a
merarim-287 <merari_287@outlook.com>
2025-09-05 16:05:13 -0600

    name on js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,8 @@
 const courseTitle = document.getElementById("course-title");
+const name = document.querySelector("main h1").textContent;
+
+const nameSpan = document.createElement("span");
+nameSpan.textContent = name;
+nameSpan.classList.add("inline-name");
+
+courseTitle.appendChild(nameSpan);
