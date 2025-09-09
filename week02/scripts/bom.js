2519377a3a270dcc9e15d8cd0cfb5b9e2e7095ed
merarim-287 <merari_287@outlook.com>
2025-09-08 18:22:51 -0600

    js

---
+++ week02/scripts/bom.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('_____');
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.ariaValueMax;
+
+deleteButton.textContent = '❌';
+li.append(deleteButton);
