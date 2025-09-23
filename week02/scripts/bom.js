c57c3f6748029a8f6e0843080d2afb7a7a545f9b
merarim-287 <merari_287@outlook.com>
2025-09-23 16:49:04 -0600

    input value + input focus

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,22 +2,21 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-button.addEventListener('click', () {
-    const chapter = input.ariaValueMax.trim();
- 
-    const li = document.createElement('li');
-    if (chapter !== '') {
-        const li = documentElement('li');
-        li.textContent = chapter;
+button.addEventListener('click', () => {
+    const chapter = input.value.trim();
 
+    if (chapter !== '') {
+        const li = document.createElement('li');
         const deleteButton = document.createElement('button');
         deleteButton.textContent = '❌';
+
+        li.textContent = chapter;
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
         deleteButton.addEventListener('click', () => {
             list.removeChild(li);
         });
-
-        li.appendChild(deleteButton);
-        list.appendChild(li);
 
         input.value = '';
         input.focus();
