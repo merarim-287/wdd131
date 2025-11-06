c91f5c3583a71a9547272aeb4a91873d9c84b1bb
merarim-287 <merari_287@outlook.com>
2025-11-06 11:56:32 -0700

    bom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('list')
+
+button.addEventListener('click', () => {
+    if (input.value.trim() === '') return;
+
+    const li = document.createElement('li');
+    li.textContent = input.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+    });
+
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    input.value = '';
+    input.focus();
+});
