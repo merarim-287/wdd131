b5410a6ecd1fdc7f2ff6f22ba39d8a917cf8786e
merarim-287 <merari_287@outlook.com>
2026-07-09 12:22:01 -0600

    working on js. the logic inside the li

---
+++ week02/scripts/bom.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#addChapter');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+button.addEventListener('click', () => {
+
+});
