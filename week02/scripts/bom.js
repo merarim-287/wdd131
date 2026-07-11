e5c77906f5a04a0a8fdd5ccd81bc9395260f5ef5
merarim-287 <merari_287@outlook.com>
2026-07-10 20:18:26 -0600

    working on js. Is still not working at this point.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,7 +2,17 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('#addChapter');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
 button.addEventListener('click', () => {
+    const chapter = input.value;
+    const li = document.createElement('li');
+    li.textContent = chapter;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+    });
 
+    li.appendChild(li);
+    input.value = '';
+    input.focus();
 });
