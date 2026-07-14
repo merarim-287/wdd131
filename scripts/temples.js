cc1afe4f3d767d6610cfc094ded583d3408ffd10
merarim-287 <merari_287@outlook.com>
2026-07-13 21:03:16 -0600

    js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,11 @@
 document.getElementById("lastModified").innerHTML = document.lastModified;
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+const mainnav = document.querySelector('.drop-down')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+})
+
